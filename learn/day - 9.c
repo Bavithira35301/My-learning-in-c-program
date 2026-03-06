2c01f15bfd43d053904e4ad2aa160c19110483b8
Bavithira35301 <bavithiravanitha@gmail.com>
2026-03-06 18:12:35 +0530

    Day - 9 completed(basic factorial program)

---
+++ learn/day - 9.c
@@ -0,0 +1,17 @@
+// basic factorial using loop
+#include <stdio.h>
+
+int main() {
+    int n,i;
+    long fact=1;
+
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+        fact=fact*i;
+
+    printf("Factorial = %ld",fact);
+
+    return 0;
+}
