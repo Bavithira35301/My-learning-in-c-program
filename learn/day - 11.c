3101b728cc0e167bb451a8b2295b7a17691bb596
Bavithira35301 <bavithiravanitha@gmail.com>
2026-03-06 18:15:49 +0530

    Day -11 completed(basic program to determine if a number is prime or not)`

---
+++ learn/day - 11.c
@@ -0,0 +1,25 @@
+//basic program to determine if a number is prime or not 
+#include <stdio.h>
+
+int main() {
+    int n,i,flag=0;
+
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    for(i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+
+    if(flag==0)
+        printf("Prime number");
+    else
+        printf("Not prime");
+
+    return 0;
+}
